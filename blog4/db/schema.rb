60d88fffdd47e3b71055128d5c6f630fc791fbee
kai_Ker <kai_Ker@buaa.edu.cn>
2024-10-27 13:59:02 +0800

    feat: add username for User and link the blog with user

--- blog4/db/schema.rb
+++ blog4/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_27_042642) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_051816) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -34,9 +36,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_27_042642) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "blogs", "users"
   add_foreign_key "comments", "blogs"
 end
