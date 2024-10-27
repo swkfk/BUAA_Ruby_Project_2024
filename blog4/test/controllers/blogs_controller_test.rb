b35f1b05fc41d5403bb13067d9884985e026d7e1
kai_Ker <kai_Ker@buaa.edu.cn>
2024-10-27 13:12:53 +0800

    fix: make the damn tests passed for user login

--- blog4/test/controllers/blogs_controller_test.rb
+++ blog4/test/controllers/blogs_controller_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class BlogsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   setup do
+    sign_in users(:one)
     @blog = blogs(:one)
   end
 
