3128bfac905153658356eff70611460c1403f3c3
kai_Ker <kai_Ker@buaa.edu.cn>
2024-11-27 14:39:57 +0800

    feat: insert the initial items into the database

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+%w[Admin Merchant Customer].each do |role_name|
+  UserRole.find_or_create_by!(name: role_name)
+end
+
+User.find_or_create_by!(name: 'admin') do |user|
+  user.name = "admin"
+  user.password = "650866740170da66899f27952fb6b7420080e025"
+end
+
+if User.find_by(name: 'admin').user_roles.find_by(name: 'Admin').nil?
+  User.find_by(name: 'admin').user_roles << UserRole.find_by(name: 'Admin')
+end
